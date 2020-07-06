b92b0e539e459463eab954f67533a02f818214aa
Artyom <danilinartem1997@gmail.com>
2020-07-06 13:02:44 +0300

    Updates

---
+++ precache-manifest.65a38b10f0986a4396d83d4b6e949e84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e028ed54578e398daade719d7506cf",
+    "url": "/react_redux/index.html"
+  },
+  {
+    "revision": "faf16ad8e235a7ab32c9",
+    "url": "/react_redux/static/js/2.0acec8ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_redux/static/js/2.0acec8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150d3ac979a71ba8ee44",
+    "url": "/react_redux/static/js/main.0546dda0.chunk.js"
+  },
+  {
+    "revision": "1aa436725fda5dd07f3d",
+    "url": "/react_redux/static/js/runtime-main.81a957ed.js"
+  }
+]);
